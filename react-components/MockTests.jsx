a9cda2fbea5ace4f3cebb3b9e80c5b56f77edffc
velvetAudacity <166835769+velvetAudacity@users.noreply.github.com>
2025-05-26 21:14:57 +0200

    package json and react

---
+++ react-components/MockTests.jsx
@@ -0,0 +1,8 @@
+import React, {useState} from "react";
+import ReactDOM from 'react-dom'
+
+function GermanTest(){
+   const [answers, setAnswers] = useState(['', '', '']);
+   const correctAnswers = ['in der', 'Im', 'weil'];
+  const [feedback, setFeedback] = useState([]);
+}
